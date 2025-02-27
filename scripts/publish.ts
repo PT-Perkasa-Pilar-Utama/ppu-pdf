b33c0ede7d0538ba43fe00701f3ac0ee3903cd57
Awal Ariansyah <awalariansyah7@gmail.com>
2025-02-27 11:24:08 +0700

    fix script publish

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -1,6 +1,14 @@
-import { cpToLib, exec } from './utils';
+import { file, write } from "bun";
+import { join } from "node:path";
+import { cpToLib, exec } from "./utils";
+
+// Get the latest pdfjs.worker
+const workerFile = await file(
+  "node_modules/pdfjs-dist/build/pdf.worker.min.mjs"
+).text();
+await write(join("./pdf.worker.min.mjs"), workerFile);
 
 // Write required files
-await Promise.all(['./README.md', './package.json'].map(cpToLib));
+await Promise.all(["./README.md", "./package.json"].map(cpToLib));
 
 await exec`cd lib && bun publish --access=public`;
